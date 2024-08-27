83c76fe99cdc5e7185db0473dfd489ac81e6a371
merb <michael.erb@erbwebdev.com>
2024-08-27 17:09:39 -0600

    feat: add new month+api

---
+++ src/lib/components/new-month.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { getYear } from 'date-fns'
+	import { returnMonth, monthsLongList, to } from '$lib/lilUtils'
+	import axios from 'redaxios'
+
+	interface fields {
+		year: number,
+		month: monthsLong
+	}
+
+	const today = new Date()
+
+	const fields:fields = {
+		year: getYear(today),
+		month: returnMonth(today, { returnType: 'long' })
+	}
+
+	async function handleSubmit () {
+		const {res, err} = await to(axios.post('/api/months', fields))
+		if (err) console.error('err: ', err)
+		console.log('res.data: ', res!.data)
+	}
+</script>
+
+<form class="get_started_form" on:submit|preventDefault={handleSubmit}>
+	<label for="">
+		<span class="label">Year</span>
+		<input type="text" bind:value={fields.year}>
+	</label>
+	<label>
+		<span class="label">Month</span>
+		<select bind:value={fields.month}>
+			{#each monthsLongList as item}
+				<option>{item}</option>
+			{/each}
+		</select>
+	</label>
+	<div class="btn_wrap __right">
+		<button class="btn" type="submit">Add</button>
+	</div>
+</form>
+
+<style lang="postcss">
+	.get_started_form {
+		margin-top: var(--size-3);
+		max-width: var(--size-96);
+	}
+</style>
---
+++ src/routes/api/months/+server.ts
@@ -0,0 +1,61 @@
+import { error, json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { to } from '$lib/lilUtils'
+import { db } from '$lib/server/db'
+import { years, months } from '@/lib/server/schema'
+
+export const POST: RequestHandler = async ({ request }) => {
+	const { res: data, err: jsonErr } = await to(request.json())
+	if (jsonErr) error(401, 'Bad request')
+	console.error('jsonErr: ', jsonErr)
+
+	console.log('data: ', data)
+	const res = await saveYearMonthDb(data)
+
+	return json(res)
+}
+
+interface returnResult {
+	year: typeof years.$inferInsert|undefined
+	month: typeof months.$inferInsert|undefined
+}
+
+async function saveYearMonthDb ({ year:yearName, month:monthName }:{ year:number, month:string }):Promise<returnResult|undefined> {
+	const result:returnResult = {
+		year: undefined,
+		month: undefined
+	}
+
+	// look for if year already exists
+	const {res: yearFromDb, err: errYearFromDb} = await to(db.query.years.findFirst({
+		where: ((years, {eq}) => eq(years.name, String(yearName)))
+	}))
+
+	console.error('errYearFromDb: ', errYearFromDb)
+	console.log('yearFromDb: ', yearFromDb)
+
+	if (!yearFromDb) {
+		const {res: resNewYear, err: errNewYear} = await to(db.insert(years).values({name: String(yearName), note: ''}).returning())
+		console.error('errNewYear: ', errNewYear)
+		console.log('resNewYear: ', resNewYear)
+		result.year = resNewYear![0]
+	} else result.year = yearFromDb
+
+	// look for if month already exists
+	const {res: monthFromDb, err: errMonthFromDb} = await to(db.query.months.findFirst({
+		where: ((months, {eq, and}) => and(eq(months.name, monthName), eq(months.yearId, result.year!.id!)))
+	}))
+
+	console.error('errMonthFromDb: ', errMonthFromDb)
+	console.log('monthFromDb: ', monthFromDb)
+
+	if (!monthFromDb) {
+		const { res: resNewMonth, err: errNewMonth } = await to(db.insert(months).values({name: monthName, yearId: result.year!.id!}).returning())
+		console.error('errNewMonth: ', errNewMonth)
+		console.log('db resMonth: ', resNewMonth)
+		result.month = resNewMonth![0]
+		return result
+	} else {
+		result.month = monthFromDb
+	}
+}
