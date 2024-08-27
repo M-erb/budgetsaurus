5d9e4a8ff051677bcec0cb7a43b376f4f7848ce2
merb <michael.erb@erbwebdev.com>
2024-08-27 17:10:21 -0600

    feat: display current years or add a new one if none

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
 	import type { PageServerData } from './$types'
+	import NewMonth from '$lib/components/new-month.svelte'
 
 	export let data:PageServerData
 	console.log('data: ', data)
+	let isEmpty = !Boolean(data.years.length)
 </script>
 
 <section class="container">
-	<h1>Welcome to the app! {JSON.stringify(data)}</h1>
+	<h1>Welcome to the app!</h1>
+	{#if isEmpty}
+		<p>Seems there is no data yet, lets get started!</p>
+		<NewMonth />
+	{:else}
+		<div class="content">
+			<ul>
+				{#each data.years as year}
+					<li>
+						<p><a href="">{year.name}</a></p>
+						<ul>
+							{#each year.months as month}
+								<li>
+									<p><a href="">{month.name}</a></p>
+								</li>
+							{/each}
+						</ul>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	{/if}
 </section>
 
 <section class="container">
