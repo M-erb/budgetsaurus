a708d3a8324a87b1f9d4892da814041f71f71d11
merb <michael.erb@erbwebdev.com>
2024-09-01 12:20:13 -0600

    feat: use table+show over or under budget

--- src/routes/app/month/[id]/+page.svelte
+++ src/routes/app/month/[id]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { PageServerData } from './$types'
-	import { centsToDollars } from '$lib/lilUtils'
+	import { centsToDollars, isNegative } from '$lib/lilUtils'
+	import ThumbsDown from '@/lib/icons/thumbs-down.svelte'
+	import ThumbsUp from '@/lib/icons/thumbs-up.svelte'
 
 	export let data:PageServerData
 </script>
@@ -24,32 +26,62 @@
 <section class="container_sm">
 	<div class="cats_report sub_area">
 		<h2 class="h4 sec_title">Categories</h2>
-		<div class="cats_list">
-			<div class="cat_item __header">
-				<div class="cat_label">
-					<span>Name</span>
-				</div>
-				<div class="cat_total">
-					<span>Spent</span>
-				</div>
-				<div class="cat_budget">
-					<span>Budget</span>
-				</div>
-			</div>
-			{#each data.monthlyReport as cat }
-				<div class="cat_item">
-					<div class="cat_label">
-						<span>{cat.categoryName}</span>
-					</div>
-					<div class="cat_total">
-						<span>{centsToDollars(cat.totalAmount)}</span>
-					</div>
-					<div class="cat_budget">
-						<span>{centsToDollars(cat.budgetAmount)}</span>
-					</div>
-				</div>
-			{/each}
-		</div>
+
+		<table class="cat_report">
+			<colgroup>
+				<col span="1" style="width: 45%">
+				<col span="1" style="width: 15%">
+				<col span="1" style="width: 15%">
+				<col span="1" style="width: 20%">
+				<col span="1" style="width: 5%">
+			</colgroup>
+			<thead>
+				<tr>
+					<td><div class="cat_label">Name</div></td>
+					<td><div class="cat_total">Spent</div></td>
+					<td><div class="cat_budget">Budget</div></td>
+					<td><div class="cat_result">Remaining</div></td>
+					<td></td>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.monthlyReport as cat }
+					{@const difference = (cat.budgetAmount ?? 0) - cat.totalAmount}
+					{@const isDiffNeg = isNegative(difference)}
+					<tr>
+						<td>
+							<div class="cat_label">
+								<span>{cat.categoryName}</span>
+							</div>
+						</td>
+						<td>
+							<div class="cat_total">
+								<span>{centsToDollars(cat.totalAmount)}</span>
+							</div>
+						</td>
+						<td>
+							<div class="cat_budget">
+								<span>{centsToDollars(cat.budgetAmount)}</span>
+							</div>
+						</td>
+						<td>
+							<div class="cat_result" class:__negative={isDiffNeg}>
+								<span>{centsToDollars(difference)}</span>
+							</div>
+						</td>
+						<td>
+							<div class="cat_icon" class:__negative={isDiffNeg}>
+								{#if isDiffNeg}
+									<ThumbsDown />
+								{:else}
+									<ThumbsUp />
+								{/if}
+							</div>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </section>
 
@@ -68,32 +100,48 @@
 		}
 	}
 
-	/* TODO: change this to a table, it just makes sense */
-	.cats_list {
-		.cat_item {
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-			gap: var(--size-4);
-			padding: var(--size-1) 0;
+	.cat_report {
+		width: 100%;
 
-			.cat_label {
-				flex: 1 1 80px;
-				font-size: var(--scale-3);
+		thead {
+			td {
+				.cat_label {
+					font-size: var(--scale-2);
+				}
+
+				.cat_total, .cat_budget, .cat_result {
+					text-align: right;
+					font-size: var(--scale-2);
+				}
 			}
+		}
 
-			.cat_total {
-				flex: 1 1 50px;
-				text-align: right;
-				font-family: var(--font-mono);
-				font-size: var(--scale-2);
-			}
+		tbody {
+			td {
+				.cat_label {
+					font-size: var(--scale-3);
+				}
 
-			.cat_budget {
-				flex: 1 1 auto;
-				text-align: right;
-				font-family: var(--font-mono);
-				font-size: var(--scale-2);
+				.cat_total, .cat_budget, .cat_result {
+					text-align: right;
+					font-family: var(--font-mono);
+					font-size: var(--scale-2);
+				}
+
+				.cat_icon {
+					text-align: right;
+					position: relative;
+					bottom: -6px;
+				}
+
+				.cat_result, .cat_icon {
+					color: var(--color-green-300);
+
+					&.__negative {
+						color: var(--color-red-300);
+					}
+				}
+
 			}
 		}
 	}
