fe2b1e16635d89cba5caac3a610ae70c8814b1d7
merb <michael.erb@spacedino.dev>
2024-09-02 14:49:52 -0600

    feat: switch back to flex

--- src/routes/app/month/[id]/+page.svelte
+++ src/routes/app/month/[id]/+page.svelte
@@ -27,61 +27,33 @@
 	<div class="sub_area">
 		<h2 class="h4 sec_title">Categories</h2>
 
-		<table class="cat_report">
-			<colgroup>
-				<col span="1" style="width: 45%">
-				<col span="1" style="width: 15%">
-				<col span="1" style="width: 15%">
-				<col span="1" style="width: 20%">
-				<col span="1" style="width: 5%">
-			</colgroup>
-			<thead>
-				<tr>
-					<td><div class="cat_label">Name</div></td>
-					<td><div class="cat_total">Spent</div></td>
-					<td><div class="cat_budget">Budget</div></td>
-					<td><div class="cat_result">Remaining</div></td>
-					<td></td>
-				</tr>
-			</thead>
-			<tbody>
-				{#each data.monthlyReport as cat }
-					{@const difference = (cat.budgetAmount ?? 0) - cat.totalAmount}
-					{@const isDiffNeg = isNegative(difference)}
-					<tr>
-						<td>
-							<div class="cat_label">
-								<span>{cat.categoryName}</span>
-							</div>
-						</td>
-						<td>
-							<div class="cat_total">
-								<span>{centsToDollars(cat.totalAmount)}</span>
-							</div>
-						</td>
-						<td>
-							<div class="cat_budget">
-								<span>{centsToDollars(cat.budgetAmount)}</span>
-							</div>
-						</td>
-						<td>
-							<div class="cat_result" class:__negative={isDiffNeg}>
-								<span>{centsToDollars(difference)}</span>
-							</div>
-						</td>
-						<td>
-							<div class="cat_icon" class:__negative={isDiffNeg}>
-								{#if isDiffNeg}
-									<ThumbsDown />
-								{:else}
-									<ThumbsUp />
-								{/if}
-							</div>
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
+		<div class="cat_report">
+			{#each data.monthlyReport as cat }
+				{@const difference = (cat.budgetAmount ?? 0) - cat.totalAmount}
+				{@const isDiffNeg = isNegative(difference)}
+				<div class="cat_item">
+					<div class="cat_col cat_name">
+						<span>{cat.categoryName}</span>
+					</div>
+					<div class="cat_col cat_num">
+						<span>{centsToDollars(cat.totalAmount)}</span>
+					</div>
+					<div class="cat_col cat_num">
+						<span>{centsToDollars(cat.budgetAmount)}</span>
+					</div>
+					<div class="cat_col cat_num __colors" class:__negative={isDiffNeg}>
+						<span>{centsToDollars(difference)}</span>
+					</div>
+					<div class="cat_col cat_icon __colors" class:__negative={isDiffNeg}>
+						{#if isDiffNeg}
+							<ThumbsDown />
+						{:else}
+							<ThumbsUp />
+						{/if}
+					</div>
+				</div>
+			{/each}
+		</div>
 	</div>
 </section>
 
@@ -122,45 +94,39 @@
 	.cat_report {
 		width: 100%;
 
-		thead {
-			td {
-				.cat_label {
-					font-size: var(--scale-2);
-				}
+		.cat_item {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			gap: var(--size-4);
 
-				.cat_total, .cat_budget, .cat_result {
-					text-align: right;
-					font-size: var(--scale-2);
-				}
-			}
-		}
+			.cat_col {
+				flex: 1 1 auto;
 
-		tbody {
-			td {
-				.cat_label {
-					font-size: var(--scale-3);
-				}
-
-				.cat_total, .cat_budget, .cat_result {
-					text-align: right;
-					font-family: var(--font-mono);
-					font-size: var(--scale-2);
-				}
-
-				.cat_icon {
-					text-align: right;
-					position: relative;
-					bottom: -6px;
-				}
-
-				.cat_result, .cat_icon {
+				&.__colors {
 					color: var(--color-green-300);
 
 					&.__negative {
 						color: var(--color-red-300);
 					}
 				}
+			}
 
+			.cat_name {
+				flex: 1 1 45%;
+				font-size: var(--scale-3);
+			}
+
+			.cat_num {
+				flex: 1 1 20%;
+				font-family: var(--font-mono);
+				text-align: right;
+				font-size: var(--scale-2);
+			}
+
+			.cat_icon {
+				flex: 1 1 5%;
+				text-align: right;
 			}
 		}
 	}
