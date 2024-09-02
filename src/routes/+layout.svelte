a8c3c72af24d081a02da1ce1e589d89cee66bfdc
merb <michael.erb@spacedino.dev>
2024-09-02 16:09:40 -0600

    fix: add mono

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
-	import '@fontsource/fira-sans/400.css';
-	import '@fontsource/fira-sans/700.css';
-	import '@fontsource/fira-sans/900.css';
-	import '@fontsource/fira-sans/400-italic.css';
-	import '@fontsource/fira-sans/700-italic.css';
-	import '@fontsource/fira-sans/900-italic.css';
+	import '@fontsource/fira-sans/400.css'
+	import '@fontsource/fira-sans/700.css'
+	import '@fontsource/fira-sans/900.css'
+	import '@fontsource/fira-sans/400-italic.css'
+	import '@fontsource/fira-sans/700-italic.css'
+	import '@fontsource/fira-sans/900-italic.css'
+	import '@fontsource/ubuntu-mono/400.css'
+	import '@fontsource/ubuntu-mono/400-italic.css'
 	import '@styles/main.pcss'
 </script>
 
