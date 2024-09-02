bb695afa04e1ec524270932baf1e38c003140956
merb <michael.erb@spacedino.dev>
2024-09-02 16:10:53 -0600

    feat: add header

--- src/routes/app/month/[id]/+page.svelte
+++ src/routes/app/month/[id]/+page.svelte
@@ -27,24 +27,42 @@
 	<div class="sub_area">
 		<h2 class="h4 sec_title">Categories</h2>
 
-		<div class="cat_report">
+		<div class="cat_report flex_table">
+			<div class="cat_item ft_row __header">
+				<div class="ft_col cat_name"></div>
+				<div class="ft_col cat_result">
+					<span class="label">Spent</span>
+				</div>
+				<div class="ft_col cat_result">
+					<span class="label">Budget</span>
+				</div>
+				<div class="ft_col cat_result">
+					<span class="label">Remaining</span>
+				</div>
+				<div class="ft_col cat_icon"></div>
+			</div>
+
 			{#each data.monthlyReport as cat }
 				{@const difference = (cat.budgetAmount ?? 0) - cat.totalAmount}
 				{@const isDiffNeg = isNegative(difference)}
-				<div class="cat_item">
-					<div class="cat_col cat_name">
+				<div class="ft_row cat_item">
+					<div class="ft_col cat_name">
+						<span class="label">Name</span>
 						<span>{cat.categoryName}</span>
 					</div>
-					<div class="cat_col cat_num">
+					<div class="ft_col cat_num cat_result">
+						<span class="label">Spent</span>
 						<span>{centsToDollars(cat.totalAmount)}</span>
 					</div>
-					<div class="cat_col cat_num">
+					<div class="ft_col cat_num cat_result">
+						<span class="label">Budget</span>
 						<span>{centsToDollars(cat.budgetAmount)}</span>
 					</div>
-					<div class="cat_col cat_num __colors" class:__negative={isDiffNeg}>
+					<div class="ft_col cat_num cat_result __colors" class:__negative={isDiffNeg}>
+						<span class="label">Remaining</span>
 						<span>{centsToDollars(difference)}</span>
 					</div>
-					<div class="cat_col cat_icon __colors" class:__negative={isDiffNeg}>
+					<div class="ft_col cat_icon __colors" class:__negative={isDiffNeg}>
 						{#if isDiffNeg}
 							<ThumbsDown />
 						{:else}
@@ -77,6 +95,8 @@
 </section>
 
 <style lang="postcss">
+	@import '@styles/mediaQueries.pcss';
+
 	.month_title {
 		text-align: center;
 	}
@@ -95,14 +115,7 @@
 		width: 100%;
 
 		.cat_item {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			gap: var(--size-4);
-
-			.cat_col {
-				flex: 1 1 auto;
-
+			.ft_col {
 				&.__colors {
 					color: var(--color-green-300);
 
@@ -113,19 +126,22 @@
 			}
 
 			.cat_name {
-				flex: 1 1 45%;
+				flex-basis: 45%;
 				font-size: var(--scale-3);
 			}
 
 			.cat_num {
-				flex: 1 1 20%;
 				font-family: var(--font-mono);
-				text-align: right;
 				font-size: var(--scale-2);
 			}
 
+			.cat_result {
+				flex-basis: 15%;
+				text-align: right;
+			}
+
 			.cat_icon {
-				flex: 1 1 5%;
+				flex-basis: 5%;
 				text-align: right;
 			}
 		}
