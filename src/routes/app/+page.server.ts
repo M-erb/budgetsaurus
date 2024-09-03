e9f71fc59a570d467f2ad5659b9f97b2f289c90b
merb <michael.erb@spacedino.dev>
2024-09-02 21:22:16 -0600

    chore: remove unneded log

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -2,8 +2,7 @@
 import type { PageServerLoad } from './$types'
 import { db } from '$lib/server/db'
 
-export const load: PageServerLoad = async ({ params }) => {
-	console.log('params: ', params)
+export const load: PageServerLoad = async () => {
 	const years = await db.query.years.findMany({
 		with: {
 			months: true
