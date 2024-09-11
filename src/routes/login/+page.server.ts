477c8791392b542e2723c592003342364677ac0e
merb <michael.erb@spacedino.dev>
2024-09-11 17:18:26 -0600

    feat: add login page+server

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,44 @@
+import type { Actions } from './$types'
+import { fail, redirect } from '@sveltejs/kit'
+import { db } from '$lib/server/db'
+import { jwtSign } from '$lib/server/jwt'
+import argon2 from '@node-rs/argon2'
+
+export const actions = {
+	login: async ({request, cookies}) => {
+		const data = await request.formData()
+		const email = data.get('email')
+		const pass = data.get('pass')
+
+		if (!email) return fail(400, {
+			success: false,
+			email,
+			missing: true,
+			message: 'Email is missing'
+		})
+
+		if (!pass) return fail(400, {
+			success: false,
+			pass,
+			missing: true,
+			message: 'Password is missing'
+		})
+
+		const user = await db.query.users.findFirst({
+			where: ((users, { eq }) => eq(users.email, String(email)))
+		})
+
+		const isGood = user ? await argon2.verify(user.pass, String(pass)) : false
+
+		if (!user || !isGood) return fail(400, {
+			success: false,
+			message: 'Incorrect email or password'
+		})
+
+		// issue signed JWT as a cookie
+		const token = await jwtSign({ id: user.id })
+		cookies.set('token', String(token), { path: '/' })
+
+		redirect(300, '/app')
+	}
+} satisfies Actions
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,55 @@
+<script lang=ts>
+	import type { ActionData } from './$types'
+	import { enhance } from '$app/forms'
+
+	export let form:ActionData
+</script>
+
+<section class="container_sm">
+	<h1>Budgetsaurus</h1>
+	<div class="sub_area form_wrap">
+		{#if form?.success === false}
+			<div class="alert __err">
+				<p class="">{form?.message}</p>
+			</div>
+		{/if}
+		<form method="POST" action="?/login" use:enhance>
+			<label>
+				<span class="label">Email</span>
+				<input name="email" type="email">
+			</label>
+			<label>
+				<span class="label">Password</span>
+				<input name="pass" type="password">
+			</label>
+
+			<div class="btn_wrap __right">
+				<button class=btn>Log in</button>
+				<!-- <button class=btn formaction="?/register">Register</button> -->
+			</div>
+		</form>
+	</div>
+</section>
+
+<style lang="postcss">
+	@import '@styles/mediaQueries.pcss';
+
+	h1 {
+		text-align: center;
+	}
+
+	.form_wrap {
+		margin-top: var(--size-12);
+		max-width: 500px;
+		margin-right: auto;
+		margin-left: auto;
+
+		.alert {
+			margin-bottom: var(--size-4);
+		}
+
+		.btn_wrap {
+			margin-top: var(--size-12);
+		}
+	}
+</style>
