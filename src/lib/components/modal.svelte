3bd0518935c17d748b0fff08c1d8aa5a9d18a770
merb <michael.erb@spacedino.dev>
2024-09-12 22:36:44 -0600

    feat: add modal

---
+++ src/lib/components/modal.svelte
@@ -0,0 +1,80 @@
+<script lang=ts>
+	import { createEventDispatcher } from 'svelte'
+	import CloseX from '$lib/icons/close-x.svelte'
+
+	const dispatch = createEventDispatcher()
+	export let showModal:boolean
+
+	let dialog:HTMLDialogElement
+
+	$: if (dialog && showModal) dialog.showModal()
+
+	function close () {
+		dialog.close()
+		dispatch('close')
+	}
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={close}
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div class="modal_inner" on:click|stopPropagation>
+		<!-- svelte-ignore a11y-autofocus -->
+		<button class=close_btn autofocus on:click|preventDefault={close}><CloseX /></button>
+		<slot />
+	</div>
+</dialog>
+
+<style lang=postcss>
+	dialog {
+		max-width: var(--width-xs);
+		border-radius: var(--radius-xl);
+		background-color: var(--color-slate-700);
+		border: 4px solid var(--color-blue-200);
+		color: var(--color-white);
+		padding: 0;
+		position: relative;
+
+		&::backdrop {
+			background-color: var(--color-slate-900);
+			opacity: .9;
+		}
+
+		& > .modal_inner {
+			padding: var(--size-7) var(--size-4) var(--size-4);
+		}
+
+		&[open] {
+			animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+		}
+
+		&[open]::backdrop {
+			animation: fade 0.2s ease-out;
+		}
+
+		.close_btn {
+			position: absolute;
+			top: var(--size-2);
+			right: var(--size-2);
+			background-color: transparent;
+			border: none;
+			padding: 0;
+			color: var(--color-slate-200);
+			cursor: pointer;
+			transition: color .3s ease-in-out;
+
+			svg {
+				width: 32px;
+				height: 32px;
+			}
+
+			&:hover {
+				color: var(--color-red-200);
+			}
+		}
+	}
+</style>
