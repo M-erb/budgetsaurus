d76ff261ae17ce3e40ce0d9425434f854d7fdae6
merb <michael.erb@erbwebdev.com>
2024-10-03 13:18:35 -0600

    fix: if no env for turso url then use in-memory

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,8 +3,10 @@ import { drizzle } from 'drizzle-orm/libsql'
 import { createClient } from '@libsql/client'
 import { env } from '$env/dynamic/private'
 
-const url = env.TURSO_URL || process.env.TURSO_URL || ''
+const url = env.TURSO_URL || process.env.TURSO_URL || ':memory:'
 const authToken = env.TURSO_AUTH || process.env.TURSO_AUTH || ''
+
+if (url === ':memory:') console.log('DB in-memory mode')
 
 const client = createClient({ url, authToken })
 export const db = drizzle(client, { schema })
