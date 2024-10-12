9a681f13dfd65604f80474278c90ef084b03c9be
merb <michael.erb@erbwebdev.com>
2024-10-12 13:37:15 -0600

    feat: WIP add update month

---
+++ src/lib/server/queries/updateMonthName.ts
@@ -0,0 +1,62 @@
+import { to, monthsLongList } from '$lib/lilUtils'
+import { db } from '$lib/server/db'
+import { years, months } from '@/lib/server/schema'
+
+interface returnResult {
+	year: typeof years.$inferInsert|undefined
+	month: typeof months.$inferInsert|undefined
+}
+
+interface input {
+	year: {
+		id: number
+	},
+	month: {
+		id: number,
+		name: string
+	}
+}
+
+export async function updateMonthName ({ year, month }: input):Promise<returnResult|undefined> {
+	/*
+	verify year.id exists
+	verify month.id exists
+	check if month.name being requested is a valid month name
+	check if this year already has the month.name being requested
+
+	if all checks out then update the month with the month.id to the new name
+	*/
+	const result:returnResult = {
+		year: undefined,
+		month: undefined
+	}
+
+	// look for if year already exists
+	const {res: yearFromDb, err: errYearFromDb} = await to(db.query.years.findFirst({
+		where: ((years, {eq}) => eq(years.name, String(yearName)))
+	}))
+
+	if (errYearFromDb) console.error('errYearFromDb: ', errYearFromDb)
+
+	if (!yearFromDb) {
+		const {res: resNewYear, err: errNewYear} = await to(db.insert(years).values({name: String(yearName), note: ''}).returning())
+		if (errNewYear) console.error('errNewYear: ', errNewYear)
+		result.year = resNewYear![0]
+	} else result.year = yearFromDb
+
+	// look for if month already exists
+	const {res: monthFromDb, err: errMonthFromDb} = await to(db.query.months.findFirst({
+		where: ((months, {eq, and}) => and(eq(months.name, monthName), eq(months.yearId, result.year!.id!)))
+	}))
+
+	if (errMonthFromDb) console.error('errMonthFromDb: ', errMonthFromDb)
+
+	if (!monthFromDb) {
+		const { res: resNewMonth, err: errNewMonth } = await to(db.insert(months).values({name: monthName, yearId: result.year!.id!}).returning())
+		if (errNewMonth) console.error('errNewMonth: ', errNewMonth)
+		result.month = resNewMonth![0]
+		return result
+	} else {
+		result.month = monthFromDb
+	}
+}
